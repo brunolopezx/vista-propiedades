244519060c224bc6b0d45a7e9a848e9615f85b35
Bruno Lopez <brunolopez164@gmail.com>
2026-05-12 16:01:43 -0300

    update archivos nombre

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Properties from "./pages/Properties";
-import PropertyDetail from "./pages/propertyDetail";
+import PropertyDetail from "./pages/PropertyDetail";
 
 function App() {
   return (
